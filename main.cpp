0d381718ee88036075bac4561707136d9071de21
lukerhoads <pumpkineater661@gmail.com>
2021-04-11 17:41:27 -0500

    4/11 - added name function

--- main.cpp
+++ main.cpp
@@ -10,22 +10,21 @@ int main(int argc, char *argv[])
 
     if (strlen(*argv) == 0) 
     { 
-        exit(400);
+        exit(1);
     };
 
     int* maxRetries;
-    char* hostedZone;
+    char* hostedZoneName;
     char* accessKey;
     char* secretKey;
     char* signature;
-    // MUST BE IPv4
-    char* redirectLocation;
+    char* redirectLocation; // IPv4 required
 
     for (int i = 1; i < argc; ++i)
     {
         if (i == 1) 
         {
-            hostedZone = argv[i];
+            hostedZoneName = argv[i];
         };
         if (i == 2)
         {
@@ -48,24 +47,50 @@ int main(int argc, char *argv[])
     bool err = false;
     for (int i = 0; i < maxRetries; ++i) 
     {
-        bool err = createRecord(hostedZone, redirectLocation, accessKey, secretKey, signature)
-        if (!err) 
+        char* hostedZoneId = getHostedZoneId(hostedZoneName, accessKey, signature);
+        bool err = createRecord(hostedZoneId, redirectLocation, accessKey, secretKey, signature);
+        if (err) 
         {
-            break;
+            std::cout << "Request failed, retrying..." << std::endl;
         };
-        std::cout << "Request failed, retrying..." << std::endl;
+        break;
     }
     
     return 0;
 }
 
 // Gets the hostedzoneID with its doman/name
-char getHostedZoneId() {
+char* getHostedZoneId(char* hostedZoneName, char* accessKey, char* signature) 
+{
+    // Time preparation
+    time_t now;
+    time(&now);
+    char buf[sizeof "2011-10-08T07:07:09Z"];
+    strftime(buf, sizeof buf, "%FT%TZ", gmtime(&now));
 
+    // Authentication preparation
+    const char *url = "https://route53.amazonaws.com/doc/2013-04-01/hostedzone";
+    const char *authorization = sprintf("AWS4-HMAC-SHA256 Credential=%s/20210409/us-east-1/route53/aws4_request, SignedHeaders=host;x-amz-date, Signature=%s", accessKey, signature);
+
+    // Request constructing and execution (very simple with cpr)
+    cpr::Response r = cpr::Get(
+                    cpr::Url{url},
+                    cpr::Header{
+                        {"X-Amz-Date", buf}, // date now unix iso format
+                        {"Authorization", authorization},
+                    });
+
+    if (r.status_code != 200) 
+    {
+        std::cout << "No hosted zone with that name!" << std::endl;
+        exit(1);
+    };
+
+    // Parse XML response, skipping truncation for now
 }
 
 // Creates the record with provided info
-bool createRecord(char* hostedZone, char* redirectLocation, char* accessKey, char* secretKey, char* signature) 
+bool createRecord(char* hostedZoneId, char* redirectLocation, char* accessKey, char* secretKey, char* signature) 
 {
     // Time preparation
     time_t now;
@@ -98,8 +123,10 @@ bool createRecord(char* hostedZone, char* redirectLocation, char* accessKey, cha
     ", hostedZone, redirectLocation);
     
     // Authentication preparation
-    const char *url = sprintf("https://route53.amazonaws.com/doc/2013-04-01/hostedzone/%s/rrset/", hostedZone);
+    const char *url = sprintf("https://route53.amazonaws.com/doc/2013-04-01/hostedzone/%s/rrset/", hostedZoneId);
     const char *authorization = sprintf("AWS4-HMAC-SHA256 Credential=%s/20210409/us-east-1/route53/aws4_request, SignedHeaders=host;x-amz-date, Signature=%s", accessKey, signature);
+
+    // Request constructing and execution (very simple with cpr)
     cpr::Response r = cpr::Post(
                     cpr::Url{url},
                     cpr::Header{
@@ -108,21 +135,13 @@ bool createRecord(char* hostedZone, char* redirectLocation, char* accessKey, cha
                         {"Content-type", "application/xml"} // or text/xml
                     },
                     cpr::Body{xmlStr});
-    std::cout << r.text << std::endl;
-}
+    
+    if (r.status_code != 200) 
+    {
+        std::cout << "Error adding A record!" << std::endl;
+        return false;
+    };
 
-// CURL *curl;
-// CURLcode res;
-// curl = curl_easy_init();
-// if(curl) {
-//     curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
-//     curl_easy_setopt(curl, CURLOPT_URL, "https://route53.amazonaws.com/doc/2013-04-01/kalinpatel.com");
-//     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-//     curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
-//     struct curl_slist *headers = NULL;
-//     headers = curl_slist_append(headers, "X-Amz-Date: 20210409T172252Z");
-//     headers = curl_slist_append(headers, "Authorization: AWS4-HMAC-SHA256 Credential=AKIAQ2OXEXVMITIVIE6A/20210409/us-east-1/route53/aws4_request, SignedHeaders=host;x-amz-date, Signature=e29ae6bcdfe4f99c72c6d1a98a6aa0cceefa517e52e6b16d7b4a932de3c336e6");
-//     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-//     res = curl_easy_perform(curl);
-// }
-// curl_easy_cleanup(curl);
+    std::cout << "Success!" << std::endl;
+    return true;
+}
